099c1d8dfba0c12c3f3dd68cfc31fa6490f2807e
Tiago Correia <tiagocorreia@vodafone.pt>
2020-02-22 18:27:31 +0000

    Added improvements to Calculator

---
+++ Calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+# Apenas para testar a Gihub CLI
+# Criar um repositorio pela linha de comando
+# Sem a necessidade de utilizar o browser.
+
+class Calculator:
+
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def soma(self):
+        return(self.num1 + self.num2)
+
+    def subtrai(self):
+        return(self.num1 - self.num2)
+
+    def multiplica(self):
+        return(self.num1 * self.num2)
+
+    def divide(self):
+        return(self.num1 / self.num2)
+
---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# Apenas para testar a Gihub CLI
+# Criar um repositorio pela linha de comando
+# Sem a necessidade de utilizar o browser.
+
+import Calculator as Calc
+
+
+def main():
+    c1 = Calc.Calculator(150,20)
+
+    print("O resultado da Soma é ->",c1.soma());
+    print("O resultado da Subtração é ->",c1.subtrai());
+    print("O resultado da Multiplicação é ->",c1.multiplica());
+    print("O resultado da Divisão é ->",c1.divide());
+
+if __name__ == "__main__":
+    main()
